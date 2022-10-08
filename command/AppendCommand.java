3ef2aa08fd1aed10ab989bea866dd775811120f3
nataliecrawford <crawfon@email.sc.edu>
2022-10-08 14:47:31 -0400

    javadoc commands for appendcommand and view command

--- command/AppendCommand.java
+++ command/AppendCommand.java
@@ -1,18 +1,34 @@
 package command;
 
+/**
+ * Append Command Class
+ * @author Natalie Crawford
+ */
+
 import java.util.Scanner;
 
 public class AppendCommand extends Command{
     private Scanner in;
+
+    /**
+     * Default constructor for appendcommand
+     * @param doc the document that will be added to
+     */
     public AppendCommand(Document doc){
         super(doc);
     }
+
+    /**
+     * executes the append action, by asking what to add 
+     * and returning if it was or not
+     * @return the value of document.append
+     */
     public String execute(){
-        Scanner sc= new Scanner(System.in);
+        Scanner in= new Scanner(System.in);
         System.out.print("Enter what you want to append : ");
-        String str= sc.nextLine();
+        String str= in.nextLine();
         String append = this.document.append(str);
+        in.close();
         return append;        
     }
-    
 }
--- command/ViewCommand.java
+++ command/ViewCommand.java
@@ -1,16 +1,24 @@
 package command;
+/**
+ * View Command Class
+ * @author Natalie Crawford
+ */
 
 public class ViewCommand extends Command {
-    
+    /**
+     * calls the view command constructor
+     * @param doc the document that will be viewed
+     */
     public ViewCommand(Document doc){
         super(doc);
     }
 
+    /**
+     * executes the desired action
+     * @return returns the visual representation of the document
+     */
     public String execute(){
         String view = this.document.view();
         return view;
-    }
-    
-
-    
+    } 
 }
