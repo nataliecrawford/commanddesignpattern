43aaea3d3a6d3f2aee259cccae10eb5bbf17bf78
Sara Hendrix <sarahendrix@Saras-MacBook-Pro-5.local>
2022-10-09 16:24:06 -0400

    javadoc comments

--- command/SaveCommand.java
+++ command/SaveCommand.java
@@ -1,11 +1,20 @@
 package command;
+/**
+ * Save command class
+ * @author sara hendrix
+ */
 public class SaveCommand extends Command{
     private Document doc;
-
+    /**
+     * constructor of SaveCommand
+     * @param doc the document being operated on
+     */
     public SaveCommand(Document doc){
         super(doc);
     }
-
+    /**
+     * tells the document to save the material added
+     */
     public String execute(){
         String save = this.document.save();
         System.out.println(save);
--- command/WriteCommand.java
+++ command/WriteCommand.java
@@ -1,12 +1,21 @@
 package command;
 import java.util.Scanner;
+/**
+ * Write Command class
+ * @author sara hendrix
+ */
 public class WriteCommand extends Command{
     private Scanner in;
-
+    /**
+     * WriteCommand constructor
+     * @param doc document being operated on
+     */
     public WriteCommand(Document doc){
         super(doc);
     }
-
+    /**
+     * gets the line being written by the user, calls the document to write 
+     */
     public String execute(){
         Scanner in= new Scanner(System.in);
         System.out.print("Enter what you want to write : ");
